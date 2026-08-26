ab0c33d4c67a1288eeffe4c48404fb036821adcb
Ezequiel <ingepasillasg@gmail.com>
2026-08-26 08:30:00 -0600

    feat: agregar navegación perezosa y componentes de página

---
+++ src/01-lazyload/pages/index.ts
@@ -0,0 +1,3 @@
+export { LazyPage1 } from "@/01-lazyload/pages/LazyPage1.tsx";
+export { LazyPage2 } from "@/01-lazyload/pages/LazyPage2.tsx";
+export { LazyPage3 } from "@/01-lazyload/pages/LazyPage3.tsx";
